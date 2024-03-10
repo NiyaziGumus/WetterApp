e060e0f71fd34e67b7313d5ec1f10b33709982a6
Niyo <ngumus14@gmail.com>
2024-03-10 15:05:37 +0100

    New weather icons added.

--- public/script.js
+++ public/script.js
@@ -1,24 +1,30 @@
-document.getElementById('searchBtn').addEventListener('click', function() {
-    const city = document.getElementById('suchfeld').value; 
+document.getElementById('searchBtn').addEventListener('click', function () {
+    const city = document.getElementById('suchfeld').value;
     fetch(`/weather?city=${city}`)
         .then(response => response.json())
         .then(data => {
-            const tempRounded = Math.round(data.main.temp); 
+            const tempRounded = Math.round(data.main.temp);
             document.getElementById('tempValue').textContent = tempRounded;
             document.getElementById('stadt').textContent = `${data.name}`;
             document.getElementById('luftfeuchtigkeit').textContent = `${data.main.humidity}%`;
             document.getElementById('windgeschwindigkeit').textContent = `${data.wind.speed} km/h`;
-            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+            let iconUrl;
+            const iconCode = data.weather[0].icon;
+            if (iconCode === "01d") {
+                iconUrl = "/img/sun.svg";
+            } else if (iconCode === "01n") {
+                iconUrl = "/img/moon.svg";
+            } else {
+                iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+            }
             document.getElementById('wetterIcon').src = iconUrl;
             document.getElementById('wetterIcon').style.display = "block";
             document.getElementById('luftIcon').style.display = "block";
             document.getElementById('windIcon').style.display = "block";
             document.getElementById('degree').style.display = "block";
-            document.querySelectorAll('.unterInfos').forEach(function(element) {
+            document.querySelectorAll('.unterInfos').forEach(function (element) {
                 element.style.display = 'flex';
             });
-            
-
         })
         .catch(error => {
             console.error('Fehler beim Abrufen der Wetterdaten:', error);
